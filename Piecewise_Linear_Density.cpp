c1c489b4704697d5a4af354fd9959989508a2ad0
svalenti52 <svalenti52@yahoo.com>
2017-08-12 13:19:05 -0700

    Made Piecewise_Linear_Density accord with the new distribution "beta".

--- Piecewise_Linear_Density.cpp
+++ Piecewise_Linear_Density.cpp
@@ -8,11 +8,10 @@
  * the C++ standard library provides the piecewise linear distribution.
  */
 
-#include <val/montecarlo/MonteCarloSim_alpha.h>
+#include <val/montecarlo/MonteCarloSim_beta.h>
 #include <val/montecarlo/Histogram.h>
+#include <val/montecarlo/Chronology.h>
 #include <fstream>
-
-using DIST = DistributionType;
 
 int main() {
 
@@ -27,33 +26,43 @@ int main() {
                                   1.0/20.0 * normalizing_constant,
                                   1.0/20.0 * normalizing_constant};
 
-    Distribution<double, DIST::PiecewiseLinearReal> distribution(
+    Distribution<double, double, std::piecewise_linear_distribution> distribution(
             intervals.begin(), intervals.end(), weights.begin(), 20);
 
     auto condition_met = [&histogram](
-            Distribution<double, DIST::PiecewiseLinearReal>& _distribution,
+            Distribution<double, double, std::piecewise_linear_distribution>& _distribution,
             double& iv,
             DRE& dre) -> bool {
 
         for ( double element : _distribution.events )
             histogram.increment_if_in_range(element);
 
+        iv = _distribution.sum() / static_cast<double>(_distribution.events.size());
+
         return true;
     };
 
-    MonteCarloSimulation<double, double, DistributionType::PiecewiseLinearReal>
+    MonteCarloSimulation<double, double, double, std::piecewise_linear_distribution>
             monteCarloSimulation(
-            200,
+            1'000'000,
             1,
             condition_met,
             distribution
     );
 
+
     monteCarloSimulation.show_distribution();
 
+    StopWatch stopWatch;
+
     monteCarloSimulation.run();
+
+    stopWatch.stop();
 
     std::ofstream histogram_file("/home/graph/histogram.txt");
     histogram_file << histogram << '\n';
 
+    monteCarloSimulation.change_message(std::string("weighted average = "));
+
+    monteCarloSimulation.print_result();
 }
